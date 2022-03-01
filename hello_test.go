2ffbbb5e1c036a6ed72c56879121587119518e21
Anushree Gupta <anushree5gupta@gmail.com>
2022-03-01 17:24:00 +0530

    [#2][Prikesh/Anushree] refactor test add empty string to hello world function

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+		assert(got, want, t)
+	})
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		assert(got, want, t)
+	})
+}
+
+func assert(got string, want string, t *testing.T) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
